dfa9d77849ce6f435d16d68578d050acc582a189
Cody Nhan Pham <87593612+codynhanpham@users.noreply.github.com>
2022-02-07 03:19:50 -0600

    fix a bug and add suggestion feature
    - Fix a bug that make the app behaves weirdly when there are multiple duplicate letters in the some words.
    - The app now provides a suggested word.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function wrongLetterFilter(arrayOfArguments, arrayToFilterFrom) {
     return arrayToFilterFrom.filter(
         function(word) {
             for (let i=0; i<arrayOfArguments.length; i++) {
-                const arr = arrayOfArguments[i].split('');
+                const arr = arrayOfArguments[i].trim().split('');
                 // Catch invalid input:
                 if (arr.length>1 || !isNaN(arr[0])) throw 'INPUT ERROR! Please input in this format: s,h,i,t,...';
 
@@ -35,12 +35,22 @@ function wrongPositionFilter(arrayOfArguments, arrayToFilterFrom) {
     return arrayToFilterFrom.filter(
         function(word) {
             for (let i=0; i<arrayOfArguments.length; i++) {
-                const arr = arrayOfArguments[i].split('');
+                const arr = arrayOfArguments[i].trim().split('');
                 // Catch invalid input:
                 if (isNaN(Number(arr[1])) || !isNaN(arr[0])) throw 'INPUT ERROR! Please input in this format: s1,h2,i3,t4,...';
 
                 // arr[1] is the position, so arr[1]-1 to get index!
-                if (!word.includes(arr[0]) || word.indexOf(arr[0]) == (arr[1]-1)) return false;
+                if (!word.includes(arr[0])) return false;
+                if (word.indexOf(arr[0]) == (arr[1]-1)) {
+                    let repeatLetterPOS = [];
+                    for (let j=arr[1]-1; j<5; j++) {
+                        if (word.indexOf(arr[0], j) == (arr[1]-1)) {
+                            repeatLetterPOS.push(j);
+                        };
+                    };
+                    if (repeatLetterPOS.length == 1) return false
+                    else if (repeatLetterPOS.includes(arr[1]-1)) return false;
+                };
             };
             return true;
         }
@@ -53,12 +63,21 @@ function correctPositionFilter(arrayOfArguments, arrayToFilterFrom) {
     return arrayToFilterFrom.filter(
         function(word) {
             for (let i=0; i<arrayOfArguments.length; i++) {
-                const arr = arrayOfArguments[i].split('');
+                const arr = arrayOfArguments[i].trim().split('');
                 // Catch invalid input:
                 if (isNaN(Number(arr[1])) || !isNaN(arr[0])) throw 'INPUT ERROR! Please input in this format: s1,h2,i3,t4,...';
 
                 // arr[1] is the position, so arr[1]-1 to get index!
-                if (word.indexOf(arr[0]) != (arr[1]-1)) return false;
+                if (!word.includes(arr[0])) return false;
+                if (word.includes(arr[0])) {
+                    let repeatLetterPOS = [];
+                    for (let j=arr[1]-1; j<5; j++) {
+                        if (word.indexOf(arr[0], j) == (arr[1]-1)) {
+                            repeatLetterPOS.push(j);
+                        };
+                    };
+                    if (!repeatLetterPOS.includes(arr[1]-1)) return false;
+                }
             };
             return true;
         }
@@ -67,6 +86,40 @@ function correctPositionFilter(arrayOfArguments, arrayToFilterFrom) {
 
 function parseToArray(commaSeperatedString) {
     return commaSeperatedString.split(',');
+    // ADD INPUT CLEANER LATER!
+    // handling cases of trailing ',' etc.
+};
+
+// Return true if any element in the input array has at least 1 duplicate, otherwise return false
+function hasDupLetter(letterArray) {
+    const dupLetters = letterArray.filter((item, index) => letterArray.indexOf(item) !== index);
+    if (dupLetters.length != 0) return true
+    else return false;
+};
+
+function hasSpecialChar(letterArray) {
+    const letterRegex = /[a-zA-Z]/;
+    let regexTrue = 0;
+    for (let i=0; i<letterArray.length; i++) { // go thru 5 character in each word
+        if (letterRegex.test(letterArray[i])) {
+            regexTrue++;
+        };
+    };
+    if (regexTrue == letterArray.length) return false;
+    return true;
+};
+
+function suggestWord(answerMatrix,guessCount) {
+    // return first one in list
+    if (guessCount >= 3 || answerMatrix.length <=5) {
+        for (let i=0; i<answerMatrix.length; i++) { // go thru word
+            if (!hasSpecialChar(answerMatrix[i])) return answerMatrix[i]
+        };
+    };
+    // return the first one with out any duplicate letter
+    for (let i=0; i<answerMatrix.length; i++) {
+        if (!hasDupLetter(answerMatrix[i]) && !hasSpecialChar(answerMatrix[i])) return answerMatrix[i];
+    };
 };
 
 const rl = readline.createInterface({
@@ -74,7 +127,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function newQuery(oldwrongLetterInput, oldwrongPositionInput, oldcorrectPositionInput) {
+function newQuery(oldwrongLetterInput, oldwrongPositionInput, oldcorrectPositionInput, guessCount) {
     rl.question('What are the wrong letters? (Input as a,b,c,d,...) ', function (wrongLetterInput) {
         rl.question('Some correct letters, but at WRONG position? (Input as a1,b2,c3,...) ', function (wrongPositionInput) {
             rl.question('And correct letters, with their correct positions? (Input as a1,b2,c3,...) ', function (correctPositionInput) {
@@ -106,6 +159,7 @@ function newQuery(oldwrongLetterInput, oldwrongPositionInput, oldcorrectPosition
                 const filteredWrongLetter = wrongLetterFilter(parseToArray(wrongLetterInput), charList);
                 const filteredCorrectLetter = wrongPositionFilter(parseToArray(wrongPositionInput), filteredWrongLetter);
                 const filteredPosition = correctPositionFilter(parseToArray(correctPositionInput), filteredCorrectLetter);
+                // filteredPosition returns a matrix of character list.
             
                 // Output
                 let answers = [];
@@ -114,13 +168,18 @@ function newQuery(oldwrongLetterInput, oldwrongPositionInput, oldcorrectPosition
                 };
                 console.log('Here is a list of possible words:');
                 console.log(answers);
+
+                let wordSuggestion = [getKeyByValue(dictionary,suggestWord(filteredPosition, guessCount))];
+                console.log('\nSuggested word for next guess:','\t',wordSuggestion);
+                console.log();
     
                 // If not finished --> input again, only have to input new information
                 rl.question('Need to narrow it down further? (y/N) ', function (yesNo) {
                     if (isNaN(Number(yesNo))) {
                         if (yesNo.toLowerCase() == 'yes' || yesNo.toLowerCase() == 'y') {
-                            console.log('\nSo now let\'s do it all again!\nYou just have to add new information!\n');
-                            newQuery(wrongLetterInput, wrongPositionInput, correctPositionInput);
+                            console.log('\n\nSo now let\'s do it all again!\nYou just have to add new information!\n');
+                            guessCount++;
+                            newQuery(wrongLetterInput, wrongPositionInput, correctPositionInput, guessCount);
                         }
                         else {
                             console.log('Nice!');
@@ -138,7 +197,8 @@ function newQuery(oldwrongLetterInput, oldwrongPositionInput, oldcorrectPosition
 };
 
 console.log('\n\t\tHere we go again, let\'s solve some Wordle problems!\n');
-newQuery('','','');
+let guessCount = 1;
+newQuery('','','',guessCount);
 
 rl.on('close', function () {
     process.exit(0);
